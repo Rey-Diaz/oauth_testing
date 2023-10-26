0d867212b13e528bef118e802ce47fd2f649e8a7
Rey-Diaz <Reinoldiaz@gmail.com>
2023-10-26 17:03:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ or implied."""
 from flask import Flask, redirect, request, render_template_string, session, url_for
 from flask_oauthlib.client import OAuth
 import requests
+import logging
 
 # Initialize Flask application
 app = Flask(__name__)
@@ -22,17 +23,20 @@ app.secret_key = '1234'  # Set a secret key for Flask sessions. Change this to a
 # Initialize OAuth for Flask
 oauth = OAuth(app)
 
+# Configure logging
+logging.basicConfig(level=logging.DEBUG)
+
 # Define Webex OAuth configuration
 webex = oauth.remote_app(
     'webex',
     consumer_key='C4fbcf5b9c6a3cee01b8079fffff52c03d4d2b6e8d7338881b7993318fc639d7e',  # Webex OAuth client ID
     consumer_secret='dd39b9daae5adb1c7a9759683858c83f92fa2e8e183bdfcd0af6019e0ba83571',  # Webex OAuth client secret
     request_token_params={'scope': 'spark:all spark:kms'},  # OAuth scopes for Webex
-    base_url='https://api.ciscospark.com/v1/',  # Base URL for Webex
+    base_url='https://webexapis.com/v1/',  # Base URL for Webex
     request_token_url=None,  # No request token URL for OAuth 2.0
     access_token_method='POST',  # HTTP method for obtaining access token
-    access_token_url='https://api.ciscospark.com/v1/access_token',  # URL to obtain access token
-    authorize_url='https://api.ciscospark.com/v1/authorize'  # URL for authorization
+    access_token_url='https://webexapis.com/v1/access_token',
+    authorize_url='https://webexapis.com/v1/authorize'
 )
 
 @app.route('/')
@@ -42,31 +46,29 @@ def index():
 
 @app.route('/login')
 def login():
-    # Redirect user to Webex for OAuth authentication
+    logging.info("Redirecting user to Webex for OAuth authentication...")
     return webex.authorize(callback=url_for('authorized', _external=True))
 
 @app.route('/login/authorized')
 def authorized():
-    # Handle the callback after Webex OAuth authentication
+    logging.info("Received callback from Webex after OAuth authentication.")
     response = webex.authorized_response()
     
-    # Check if the authentication was successful
     if response is None or response.get('access_token') is None:
+        logging.error(f"Authentication failed. Reason: {request.args['error_reason']}, Error: {request.args['error_description']}")
         return 'Access denied: reason={} error={}'.format(
             request.args['error_reason'],
             request.args['error_description']
         )
 
-    # Store the access token in the session
+    logging.info("Authentication successful. Storing access token in session.")
     session['webex_token'] = (response['access_token'], '')
     
-    # Here, you can use the access token to make authorized requests to Webex API.
-    # For example, to get user details:
     headers = {
         'Authorization': f"Bearer {response['access_token']}"
     }
     user_info = requests.get('https://api.ciscospark.com/v1/me', headers=headers).json()
-    print(user_info)  # Print user details for debugging
+    logging.debug(f"User details: {user_info}")
 
     return 'Logged in successfully!'
 
